5b9ccf36ee6dde26552058f711ffc363029cec2b
unknown <vishudhiman1900@gmail.com>
2022-11-05 11:43:46 +0530

    code pushed

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"github.com/sherlyc/golang-gin/jokes"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+
+	// Our API will consist of just two routes
+	// /jokes - which will retrieve a list of jokes a user can see
+	// /jokes/like/:jokeID - which will capture likes sent to a particular joke
+	api.GET("/jokes", jokes.JokeHandler)
+	api.POST("/jokes/like/:jokeID", jokes.LikeJoke)
+
+	// Start and run the server
+	router.Run(":3000")
+}
